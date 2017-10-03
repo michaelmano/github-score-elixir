a4e16bda87136ecd79a6503076008f08006b734a
Michael Mano <michael.mano26@gmail.com>
2017-10-04 09:02:56 +1000

    Cleans up the getScore functions

--- lib/github.ex
+++ lib/github.ex
@@ -1,6 +1,6 @@
 defmodule Github do
-  def score do
-    getUsername()
+  def score(username \\ IO.gets "Github username: ") do
+    username
     |> String.replace_trailing("\n", "")
     |> buildURL
     |> makeRequest
@@ -10,12 +10,8 @@ defmodule Github do
     |> returnScore
   end
 
-  def getUsername do
-    IO.gets "Github username: "
-  end
-
   def buildURL(username) do
-    "https://api.github.com/users/" <> username <>"/events"
+    "https://api.github.com/users/" <> username <> "/events"
   end
 
   def makeRequest(url) do
@@ -26,21 +22,11 @@ defmodule Github do
     Enum.map(data, fn(x) -> getScore(x["type"]) end)
   end
 
-  def getScore(data) when data == "PushEvent" do
-    5
-  end
-  def getScore(data) when data == "CreateEvent" do
-    4
-  end
-  def getScore(data) when data == "IssuesEvent" do
-    3
-  end
-  def getScore(data) when data == "CommitEvent" do
-    2
-  end
-  def getScore(data) do
-    1
-  end
+  def getScore(data) when data == "PushEvent", do: 5
+  def getScore(data) when data == "CreateEvent", do: 4
+  def getScore(data) when data == "IssuesEvent", do: 3
+  def getScore(data) when data == "CommitEvent", do: 2
+  def getScore(data), do: 1
 
   def returnScore(data) do
     "Your Github score is #{Enum.sum(data)}"
